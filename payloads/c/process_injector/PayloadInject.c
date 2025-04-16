c4393799ac63d2044dc7cd838caab2276a5e2dd1
danisqxas <mcdw.danizt@gmail.com>
2025-04-16 14:07:03 -0300

    Reorganizar estructura: mover payload C a subdirectorio 'process_injector'

---
+++ payloads/c/process_injector/PayloadInject.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <resolv.h>
+#include <curl/curl.h>
+#include <sys/sysctl.h>
+#include <mach/mach.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+
